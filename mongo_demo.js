e18d7827f467adc8efde1a5ec5bb61e2097a1413
support <support@eatj.com>
2016-02-13 11:38:48 -0800

    Modified

--- mongo_demo.js
+++ mongo_demo.js
@@ -1,6 +1,16 @@
 
 var mongoose = require('mongoose');
+
+//Please find your mongo DB URL in http://nodejs.eatj.com/account.jsp
+var mongodbURI = 'put your mongo db URL here';
+
+if(mongodbURI == 'put your mongo db URL here') 
+{
+	console.log('Please find your mongo DB URI at http://nodejs.eatj.com/account.jsp. Put the URI in mongo_demo.js');
+	process.exit();
+}
 mongoose.connect('mongodb://tcai:gcagcai@homeip/gcai');
+
 var db = mongoose.connection;
 db.on('error', console.error.bind(console, 'connection error:'));
 db.once('open', function() {
